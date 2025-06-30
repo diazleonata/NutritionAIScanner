d8c728a6846ad0b1a79e0bc3f94325a35d91ad93
diazleonata <diazleonata@outlook.com>
2025-07-01 05:48:37 +0700

    fix minor bug

--- app.config.js
+++ app.config.js
@@ -1,61 +1,58 @@
-import 'dotenv/config';
+import "dotenv/config";
 
 /** @type {import('@expo/config').ExpoConfig} */
 export default () => ({
-  expo: {
-    name: "Teman Gizi",
-    slug: "temangizi",
-    version: "1.0",
-    orientation: "portrait",
-    icon: "./assets/images/icon.png",
-    scheme: "temangizi",
-    userInterfaceStyle: "automatic",
-    newArchEnabled: true,
+    expo: {
+        name: "Teman Gizi",
+        slug: "temangizi",
+        version: "1.0",
+        orientation: "portrait",
+        icon: "./assets/images/icon.png",
+        scheme: "temangizi",
+        userInterfaceStyle: "automatic",
+        newArchEnabled: true,
 
-    extra: {
-      SUPABASE_URL: process.env.SUPABASE_URL,
-      SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
-      API_URL: process.env.API_URL,
-      eas: {
-        projectId: "0f95a52c-484b-4922-aefc-feea28099c55",
-      },
-    },
-
-    android: {
-      adaptiveIcon: {
-        foregroundImage: "./assets/images/icon.png",
-        backgroundColor: "#ffffff",
-      },
-      package: "com.ninetynines.temangizi",
-      permissions: ["android.permission.CAMERA"],
-    },
-
-    plugins: [
-      [
-        "expo-router",
-      ],
-      [
-        "expo-splash-screen",
-        {
-          image: "./assets/images/icon.png",
-          imageWidth: 200,
-          resizeMode: "contain",
-          backgroundColor: "green",
+        android: {
+            adaptiveIcon: {
+                foregroundImage: "./assets/images/icon.png",
+                backgroundColor: "#ffffff"
+            },
+            package: "com.ninetynines.temangizi",
+            permissions: ["android.permission.CAMERA"]
         },
-      ],
-      [
-        "expo-camera",
-        {
-          cameraPermission: "Allow $(PRODUCT_NAME) to access your camera to scan nutrition labels.",
-        },
-      ],
-      [
-        "expo-web-browser"
+
+        plugins: [
+            "expo-router",
+            [
+                "expo-splash-screen",
+                {
+                    image: "./assets/images/splash-icon.png",
+                    imageWidth: 200,
+                    resizeMode: "contain",
+                    backgroundColor: "#ffffff"
+                }
+            ],
+            [
+                "expo-camera",
+                {
+                    cameraPermission:
+                        "Allow $(PRODUCT_NAME) to access your camera to scan nutrition labels."
+                }
+            ],
+            "expo-web-browser"
         ],
-    ],
 
-    experiments: {
-      typedRoutes: true,
-    },
-  },
+        experiments: {
+            typedRoutes: true
+        },
+
+        extra: {
+            SUPABASE_URL: process.env.SUPABASE_URL,
+            SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
+            API_URL: process.env.API_URL,
+            eas: {
+                projectId: "0f95a52c-484b-4922-aefc-feea28099c55"
+            }
+        }
+    }
 });
